8605fb340218ff950ec0ce87ffab7f6f04c5b073
MQuaresma <miguelquaresma97@gmail.com>
2019-04-09 18:56:11 +0100

    Placeholders for instructions

--- eIDAS/main.js
+++ eIDAS/main.js
@@ -1,2 +1,19 @@
+const node_sign = require('node-signpdf');
+const fs = require('fs');
 const PDFDocument = require('pdfkit');
 const crypto = require('cryptography');
+
+signer = new node_sign.SignPdf();
+
+//TODO: read file and cert path from command line 
+pdf = fs.readFileSync('/Users/miguelq/Downloads/teste.pdf');
+cert = fs.readFileSync('/Users/miguelq/Downloads/certificate.p12');
+
+//TODO: add signature placeholder to pdf
+
+const signedPdf = signer.sign(
+    pdf_with_placeholder,
+    cert
+)
+
+//TODO: write signed pdf to filesystem
