f575018fa7258cf6258177b6956ed6d6c2405dc8
MQuaresma <miguelquaresma97@gmail.com>
2019-04-10 08:51:50 +0100

    Dirty code for CLI usage

--- eIDAS/main.js
+++ eIDAS/main.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const node_sign = require('node-signpdf');
 const fs = require('fs');
 const PDFDocument = require('pdfkit');
@@ -5,15 +6,52 @@ const crypto = require('cryptography');
 
 signer = new node_sign.SignPdf();
 
-//TODO: read file and cert path from command line 
-pdf = fs.readFileSync('/Users/miguelq/Downloads/teste.pdf');
-cert = fs.readFileSync('/Users/miguelq/Downloads/certificate.p12');
+function load_files(args){
+    pdf_index = args.indexOf("--file") + 1;
+    cert_index = args.indexOf("--cert") + 1;
 
-//TODO: add signature placeholder to pdf
+    // Check if cert and pdf file exist
+    if(pdf_index && cert_index){
+        fs.stat(args[pdf_index], function(err, stat){
+            if(!err){
+                pdf = fs.readFileSync(args[pdf_index]);
+                fs.stat(args[cert_index], function(err, stat){
+                    if(!err){
+                        cert = fs.readFileSync(args[cert_index]);
+                        sign_pdf(pdf, cert);
+                    }else{
+                        usage();
+                    }
+                });
+            }else{
+                usage();
+            }
+        });
+    }else{
+        usage();
+    }
+}
 
-const signedPdf = signer.sign(
-    pdf_with_placeholder,
-    cert
-)
+function process_args(){
+    if(process.argv.includes("-h"))
+        usage();
+    else 
+        load_files(process.argv.slice(2,));
+}
 
-//TODO: write signed pdf to filesystem
+function usage(){
+    console.log("Usage: ./main.js [--file pdf_file] [--cert cert_file]")
+}
+
+function sign_pdf(pdf, cert){
+    //TODO: add signature placeholder to pdf
+
+    const signedPdf = signer.sign(
+        pdf_with_placeholder,
+        cert
+    )
+
+    //TODO: write signed pdf to filesystem
+}
+
+process_args();
